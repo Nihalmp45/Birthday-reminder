0205562a55de67d164cdd48af2568558e13dd9ef
Nihal Abdul Khader MP <nihalmp45@gmail.com>
2022-11-01 19:41:55 +0530

    birthday reminder

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
+function App() {
+  const [people,setPeople]=useState(data)
+
+  const clearAll=()=>{
+    setPeople([])
+  }
+  const addAll=()=>{
+    setPeople(data)
+  }
+
+  return(
+     <main>
+      <section className='container'>
+         <h3>{people.length} birthdays today</h3>
+         <List people={people}/>
+         <button onClick={clearAll}>clear all</button>
+         <button onClick={addAll}>Add all</button>
+      </section>
+     </main>
+  )
+}
+
+export default App;
